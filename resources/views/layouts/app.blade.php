a3e0837b49c6595503fb2e04dd2f520e6989bc9d
Furlanets <fernando-cp1@hotmail.com>
2022-11-27 18:57:35 -0300

    niannap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ESCOLINHA DO XASTRE</title>
+    <title>ESCOLINHA DO XASTRE</title> 
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
