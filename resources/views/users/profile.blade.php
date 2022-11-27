cabb00974815259919592089b48b0d160b6886ff
Luccavco <111525582+Luccavco@users.noreply.github.com>
2022-11-27 02:57:58 -0300

    mostrar perfil

---
+++ resources/views/users/profile.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<p>{{ $user->name }}</p>
+<p>{{ $user->username }}</p>
+<p>{{ $user->email }}</p>
+<p>{{ $user->CPF }}</p>
+<p>{{ $user->filme }}</p>
+<p>{{ $user->endereço }}</p>
+<p>{{ $user->created_at }}</p>
+<p>{{ $user->updated_at }}</p>
+
+@if($user->permissions == 1)
+Aluno
+@endif
+@if($user->permissions == 2)
+Professor
+@endif
+@if($user->permissions == 3)
+Secretaria
+@endif
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Auth::routes();
 Route::get('/cursos', [App\Http\Controllers\CursosController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/perfil/{id}', [App\Http\Controllers\UserControllers::class, 'show']);
+
