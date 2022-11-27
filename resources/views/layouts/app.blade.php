ca1757745e7ab0cb67d3406ad166e1cc65133fe3
joaogalo <110982939+joaogalo@users.noreply.github.com>
2022-11-27 17:48:40 -0300

    home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ROVENTINISTORE</title>
+    <title>ESCOLINHA DO XASTRE</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -22,7 +22,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    ROVENTINISTORE
+                    ESCOLINHA DO XASTRE
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>sla</h1>
+
+<h1><font size=20 face='Nunito'><span style='color:blue;'>&nbsp;&nbsp;&nbsp;&nbsp;Escolinha do Xastre</span></font></h1>
+<h1><font size=5 face='Verdana'>&nbsp;- No canto superior direito esta a parte de login e caso nao tenha uma conta se registre.</span></font></h1>
+<h1><font size=5 face='Verdana'>&nbsp;- Depois disso entre em cursos e assim escolha qual voce quer participar. </font></h1>
+<h1><font size=5 face='Verdana'>&nbsp;- Aproveite os cursos e bom aprendizado!</font></h1>
+
+<img src="/img/escola.png" alt="Banner">
+
 @endsection
