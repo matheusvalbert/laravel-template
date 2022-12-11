2eebb5a675d8c9fb1a330b69d568c7584e9c1019
jvrov <111283496+jvrov@users.noreply.github.com>
2022-12-11 18:06:58 -0300

    ta top

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -4,10 +4,10 @@
 
 
 @foreach ($cursos as $curso)
- <p><font size=10 face='Nunito'>Curso: {{ $curso->name }} </p>
- <a href="/cursos/{{$curso->id}}"> Saiba mais</a>
+ <p><font size=10 face='Nunito'><span style='color:rgb(255, 255, 255);'>Curso: {{ $curso->name }} </p>
+ <a href="/cursos/{{$curso->id}}">Saiba mais</a>
 
 @endforeach
-<body style='background-color:rgb(255, 255, 255)'>
+
 
 @endsection
